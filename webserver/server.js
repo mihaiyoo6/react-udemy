838724cfb12922d6f7e1079ec93a04f2db80293a
Mijea Mihai <mmihai@suitsupply.com>
2015-11-24 19:39:12 +0100

    setup basic server

---
+++ webserver/server.js
@@ -0,0 +1,20 @@
+var express = require('express'),
+	path = require('path'),
+	bodyParser = require('body-parser');
+
+var app = express();
+
+//set port
+app.set('port', (process.env.PORT || 3000));
+
+//set static path
+app.use(express.static(path.join(__dirname, 'client')));
+
+//BOdyPArser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+//Start server
+app.listen(app.get('port'), function(){
+	console.log('Server has Started : http:localhost:%s', app.get('port'));
+});
