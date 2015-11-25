7f1d4c5c0b5b4923f537b01f2de8857c9d0e1f56
Mijea Mihai <mmihai@suitsupply.com>
2015-11-25 09:40:18 +0100

    add products api

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express = require('express'),
 	path = require('path'),
-	bodyParser = require('body-parser');
+	bodyParser = require('body-parser'),
+	fs = require('fs');
 
 var app = express();
 
+var PRODUCTS_FILE = path.join(__dirname, '/data/products.json');
 //set port
 app.set('port', (process.env.PORT || 3000));
 
@@ -19,6 +21,28 @@ app.use('/state', express.static(path.join(__dirname, 'nav')));
 //BOdyPArser Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+//get Products
+app.get('/api/products', function(req, res){
+	fs.readFile(PRODUCTS_FILE, function(err, data){
+		res.setHeader('Cache-Control', 'no-cache');
+		res.json(JSON.parse(data));
+	});
+});
+
+//add products
+app.post('/api/products', function(req, res){
+	fs.readFile(PRODUCTS_FILE, function(err, data){
+		var products = JSON.parse(data);
+		products.push(req.body);
+		fs.writeFile(PRODUCTS_FILE, JSON.stringify(products, null, 3), function(err){
+			res.setHeader('Cache-Control', 'no-cache');
+			res.json(products);
+		});
+	});
+});
+
+
 
 //Start server
 app.listen(app.get('port'), function(){
