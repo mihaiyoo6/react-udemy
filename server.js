1b46544016e50a9fd17b79fe90ab79dcbf58ea2d
Mijea Mihai <mmihai@suitsupply.com>
2015-11-25 19:54:37 +0100

    restructure client and add min versions of react + jQuery

--- server.js
+++ server.js
@@ -13,11 +13,11 @@ app.set('port', (process.env.PORT || 3000));
 app.use('/', express.static(path.join(__dirname, 'client')));
 app.use('/libs', express.static(path.join(__dirname, 'libs')));
 app.use('/styles', express.static(path.join(__dirname, 'styles')));
-app.use('/first', express.static(path.join(__dirname, 'firstComponent')));
-app.use('/form', express.static(path.join(__dirname, 'regForm')));
-app.use('/life', express.static(path.join(__dirname, 'lifeCycle')));
-app.use('/props', express.static(path.join(__dirname, 'props')));
-app.use('/state', express.static(path.join(__dirname, 'nav')));
+app.use('/first', express.static(path.join(__dirname, 'client/firstComponent')));
+app.use('/form', express.static(path.join(__dirname, 'client/regForm')));
+app.use('/life', express.static(path.join(__dirname, 'client/lifeCycle')));
+app.use('/props', express.static(path.join(__dirname, 'client/props')));
+app.use('/state', express.static(path.join(__dirname, 'client/nav')));
 //BOdyPArser Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
